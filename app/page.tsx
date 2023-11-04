04ff459f8544e8e48920b85ed4e3173096036307
Yousef <yousefsameh24@gmail.com>
2023-11-04 14:24:46 +0200

    Add mobile version screenshots to dashboard project.

--- app/page.tsx
+++ app/page.tsx
@@ -35,6 +35,13 @@ export default function Page() {
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop and mobile versions"
           />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="md:hidden"
+            alt="Screenshots of the dashboard project showing mobile versions"
+          />
         </div>
       </div>
     </main>
